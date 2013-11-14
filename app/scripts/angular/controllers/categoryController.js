8925e688eec416fa99e41c98cc117df7c3330d2b
sytanta <sytanta@yahoo.com>
2013-11-15 00:41:25 +0700

    partial #27

--- app/scripts/angular/controllers/categoryController.js
+++ app/scripts/angular/controllers/categoryController.js
@@ -178,7 +178,7 @@ function categoryController($scope, $cookieStore, $filter, $modal) {
   }
   
   $scope.generateFrontEndLink = function(category) {
-  	return $scope.config.appStage + $scope.Store.URI + '/';
+  	return $scope.config.appStage + '/#/' + $scope.Store.URI + '/';
   }
 }
 
