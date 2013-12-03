93601cc876977b235bb4473ee1f81478bc6c7d46
sytanta <sytanta@yahoo.com>
2013-12-03 22:10:21 +0700

    finish #28

--- app/scripts/angular/directives/checkUniqueURI.js
+++ app/scripts/angular/directives/checkUniqueURI.js
@@ -2,23 +2,33 @@
 // Check URI uniqueness (category name, event name,...)
 // Ex: <input name="Category_Name" type="text"
 //       ng-model="Category.Name"
-//       check-uniqueuri
+//       check-uniqueuri="Category"
 //       required />
 //
 // Available error pointers:
 // - modelCtrl.$setValidity('uniqueURI', false)	       : URI is already used
-// - modelCtrl.$setValidity('finishGettingURI', false) : not yet check URI uniqueness
+// - modelCtrl.$setValidity('finishGettingURI', false) : not yet done checking URI uniqueness
+// - modelCtrl.$setValidity('startGettingURI', false)  : start checking URI (for "Checking..." message display)
+//
+// This directive will work with objects having "CustomURI" property
+// like "Category, Event, Ticket,..."
 //
 
 azureTicketsApp
   .directive(
     'checkUniqueuri',
-    ['modelService',
-    function(modelService) {
+    ['modelService', '$parse',
+    function(modelService, $parse) {
       return {
-        restrict : 'A',
+        restrict: 'A',
         require: 'ngModel',
-        link : function($scope, $element, $attr, modelCtrl) {
+        link: function($scope, $element, $attr, modelCtrl) {
+        	
+        	var objToCheckURI = $parse($attr.checkUniqueuri)($scope);
+        	// If we are updating an object, get a URI copy for later comparison
+        	if (objToCheckURI && objToCheckURI.Key) {
+        		var oldURI = objToCheckURI.CustomURI.URI;
+        	}
         	
         	// These functions will fire
         	// for every input change
@@ -30,7 +40,7 @@ azureTicketsApp
         	  	// If input is empty
         	  	// Not use 'blur' event for this
         	  	// because there's no name/URI to check
-        	  	// System responds right away
+        	  	// And system needs to respond right away
         	  	if (!$element.val()) {
         	  	  modelCtrl.$setValidity('uniqueURI', true);
         	  	  modelCtrl.$setValidity('finishGettingURI', true);
@@ -48,19 +58,36 @@ azureTicketsApp
         	        ? angular.lowercase(objName.replace(/[^a-z0-9\-]{1,}/gi, '-'))
         	        : '';
         	  
+        	  $scope.$apply(function() {
+        	  	if (URI) {
+        	      modelCtrl.$setValidity('startGettingURI', false);
+        	    } else {
+        	    	modelCtrl.$setValidity('startGettingURI', true);
+        	    }
+        	  })
+        	  
+        	  // Start getting URI
         	  if (URI) {
           	  modelService.checkUniqueURI($scope.storeKey, URI).then(
         	      function(returnedURI) {
         	        // Ensure value that being checked hasn't changed
         	        if (objName == $element.val()) {
-        	          if (returnedURI && returnedURI.ItemKey) { 
-        	            modelCtrl.$setValidity('uniqueURI', false);
-        	            modelCtrl.$setValidity('finishGettingURI', true);
+        	        	// If there's already an existing URI
+        	          if (returnedURI && returnedURI.ItemKey) {
+        	          	if (oldURI && oldURI == returnedURI.URI) {
+        	          	  modelCtrl.$setValidity('uniqueURI', true);
+        	          	} else {
+        	          		modelCtrl.$setValidity('uniqueURI', false);
+        	          	}
+        	          // Or not yet
         	          } else {
         	          	modelCtrl.$setValidity('uniqueURI', true);
-        	          	modelCtrl.$setValidity('finishGettingURI', true);
         	          }
         	        }
+        	        
+        	        // Notify that getting URI is done
+        	        modelCtrl.$setValidity('finishGettingURI', true);
+        	        modelCtrl.$setValidity('startGettingURI', true);
         	      }, function(err) {
         	        $scope.error.log(err);
         	      }
